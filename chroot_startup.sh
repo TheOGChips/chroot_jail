d23e04481e8c337d0ba0263ca0db23baa10627d5
TheOGChips <swindell.christian.g@gmail.com>
2021-07-19 09:28:09 -0500

    Added mount command to fix Internet connectivity at different locations.

--- chroot_startup.sh
+++ chroot_startup.sh
@@ -2,4 +2,5 @@
 
 sudo mount -t proc proc "$1"/proc
 sudo mount -t devpts devpts "$1"/dev/pts
+sudo mount --bind /etc/resolv.conf "$1"/etc/resolv.conf	# for Internet connection
 sudo chroot "$1" /etc/init.d/ssh restart
